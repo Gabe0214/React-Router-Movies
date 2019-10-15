1bf189888f47ed07244b2cf5b10da27de55e8fb3
Gabriel Anguiano <anguianog97@gmail.com>
2019-10-15 14:40:32 -0700

    hit mvp

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {NavLink} from 'react-router-dom'
+import MovieCard from './MovieCard'
+
+
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -22,30 +26,17 @@ const MovieList = props => {
     <div className="movie-list">
       {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
+       
       ))}
     </div>
   );
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+    <NavLink to ={`/movies/${movie.id}`}>
+    <MovieCard movie= {movie} />
+    </NavLink>
   );
 }
 
